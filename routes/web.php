27297dd35316b54ed2fb972f0b98d7ca699c22bf
Felipe Calogero <felipe.augusto@bussolasocial.com.br>
2025-07-25 10:40:25 -0300

    changed routes for restfull best practices and changed variable name in front

--- routes/web.php
+++ routes/web.php
@@ -3,10 +3,19 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\DespesaController;
 
-Route::get('/', [DespesaController::class, 'index'])->name('despesas.index');
-Route::get('/despesas/criar', [DespesaController::class, 'create'])->name('despesas.create');
-Route::get('/despesas/editar/{id}', [DespesaController::class, 'edit'])->name('despesas.edit');
+//Route::get('/', [DespesaController::class, 'index'])->name('despesas.index');
+//Route::get('/despesas/criar', [DespesaController::class, 'create'])->name('despesas.create');
+//Route::get('/despesas/editar/{id}', [DespesaController::class, 'edit'])->name('despesas.edit');
+//
+//Route::post('/despesas', [DespesaController::class, 'store'])->name('despesas.store');
+//Route::put('/despesas/atualizar/{id}', [DespesaController::class, 'update'])->name('despesas.update');
+//Route::delete('/despesas/deletar/{id}', [DespesaController::class, 'destroy'])->name('despesas.destroy');
 
+
+
+Route::get('/despesas', [DespesaController::class, 'index'])->name('despesas.index');
+Route::get('/despesas/criar', [DespesaController::class, 'create'])->name('despesas.create');
 Route::post('/despesas', [DespesaController::class, 'store'])->name('despesas.store');
-Route::put('/despesas/atualizar/{id}', [DespesaController::class, 'update'])->name('despesas.update');
-Route::delete('/despesas/deletar/{id}', [DespesaController::class, 'destroy'])->name('despesas.destroy');
+Route::get('/despesas/{id}/editar', [DespesaController::class, 'edit'])->name('despesas.edit');
+Route::put('/despesas/{id}', [DespesaController::class, 'update'])->name('despesas.update');
+Route::delete('/despesas/{id}', [DespesaController::class, 'destroy'])->name('despesas.destroy');
